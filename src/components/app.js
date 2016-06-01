708eb37a625d20beb86c11bac5a83cd695963895
Danny Rizko <drizko07@gmail.com>
2016-06-01 14:52:25 -0700

    Implements react-router

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 
+const Header = () => {
+  return <div>This is the Header</div>
+}
+
+const Footer = () => {
+  return <div>This is the Footer</div>
+}
+
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <Header />
+        {this.props.children}
+        <Footer />
+      </div>
     );
   }
 }
