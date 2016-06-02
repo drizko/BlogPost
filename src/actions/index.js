9a8d4d6f352fe6eb416089865bd6c3042b04f2f1
Danny Rizko <drizko07@gmail.com>
2016-06-02 16:25:39 -0700

    Implements deletePost

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,6 +6,7 @@ const API_KEY = '?key=drizko';
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
 export const FETCH_POST = 'FETCH_POST';
+export const DELETE_POST = 'DELETE_POST';
 
 export function fetchPosts(){
   const request = axios.get(`${rootURL}/posts${API_KEY}`);
@@ -30,6 +31,15 @@ export function fetchPost(id){
 
   return {
     type: FETCH_POST,
+    payload: request
+  }
+}
+
+export function deletePost(id){
+  const request = axios.delete(`${rootURL}/posts/${id}${API_KEY}`);
+
+  return {
+    type: DELETE_POST,
     payload: request
   }
 }
